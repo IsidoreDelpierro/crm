b1f386db430eece712a1bea6bd6c61294414a780
Isidore DelPierro <fontadore@gmail.com>
2022-03-03 13:05:30 +0100

    Two - URLs & Views
    1) Routing from project to app and within app 
    2) home, products & customers views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse("home")
+
+def products(request):
+    return HttpResponse("Products page")
+
+def customers(request):
+    return HttpResponse("Customers page")
