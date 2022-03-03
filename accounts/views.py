091df1a9ed7e82e0c19ca312bd9c2ff538700465
Isidore DelPierro <fontadore@gmail.com>
2022-03-03 14:39:30 +0100

    Three: Templates & Inheritance
    1) Render to templates instead of HttpResponse 
    2) Bootstrap 
    3) Layouts for customer.html, products.html, main.html, etc

--- accounts/views.py
+++ accounts/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("home")
+    return render(request, 'accounts/dashboard.html', {})
 
 def products(request):
-    return HttpResponse("Products page")
+    return render(request, 'accounts/products.html', {})
 
 def customers(request):
-    return HttpResponse("Customers page")
+    return render(request, 'accounts/customer.html', {})
