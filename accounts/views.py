f4bb6987312f601348d7a9b2f8d24dcf66e45195
Isidore DelPierro <fontadore@gmail.com>
2022-03-04 15:47:23 +0100

    Eight: Renderign Data To Templates - Template Tags
    1) Template tags (Django - like statements within html templates) 
    
    2) Output products to products.html 
    
    3) Output info to dashboard.html

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def home(request):
-    return render(request, 'accounts/dashboard.html', {})
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    context = {'orders': orders, 'customers': customers,
+            'pending': pending, 'delivered': delivered,
+            'total_orders': total_orders}
+
+    return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
-    return render(request, 'accounts/products.html', {})
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'accounts/products.html', context)
 
 def customers(request):
     return render(request, 'accounts/customer.html', {})
